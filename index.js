3e3d5a1bac929aa2f1b352d31a85c9db1c2fb57d
Md Antor Mia <your-email@gmail.com>
2026-05-21 20:06:18 +0600

    database fixed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import dns from 'node:dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 import express from "express";
 import cors from "cors";
 import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
